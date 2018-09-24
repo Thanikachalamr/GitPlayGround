4185cd382b97efeb7926e8f6949540e612969898
Thanikachalam Ramalingam <thanikachalamr@smartek21.com>
2018-09-24 16:56:50 +0530

    This will add two numbers and print it
    Program will be adding two numbers and the same will be displayed as
    part of welcome.go execution.
    
    Ticket: DSH475

--- Welcome.go
+++ Welcome.go
@@ -1,7 +1,11 @@
 package main
 
 import "fmt"
+import (
+	L "./lib"
+)
 
 func main() {
 	fmt.Printf("hello, Smartek\n")
+	fmt.Println("Sum of Two Numbers: ", L.Add())
 }
---
+++ lib/addnumbers.go
@@ -0,0 +1,9 @@
+package mainLib
+
+// "Adding two numbers"Add
+func Add() int {
+	var a = 10
+	var b = 20
+
+	return a + b
+}
